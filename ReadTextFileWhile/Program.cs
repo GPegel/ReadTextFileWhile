12b6d0d7e87ed0d9837c570197d185fe10e61784
Gerhard Pegel <gpegel@gmail.com>
2015-09-12 21:13:17 +0200

    My first C# console application about reading data from a text file

---
+++ ReadTextFileWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTextFileWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("Values.txt");
+            string Line = "";
+
+            while (Line != null)
+            {
+                Line = myReader.ReadLine();
+                if (Line != null)
+                    Console.WriteLine(Line);
+            }
+            myReader.Close();
+            Console.ReadLine();
+        }
+    }
+}
+//  To make use of StreamReader you need to add the System.IO namespace! And to make this project run,
+//  set the property called 'Copy to Output Directory' of the Values.txt file to 'Copy Always'
