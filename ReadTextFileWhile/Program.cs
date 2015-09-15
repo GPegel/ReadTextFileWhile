9394423eb7201cd0a619afcd4698f8d25fbc28e9
Gerhard Pegel <gpegel@gmail.com>
2015-09-15 22:06:25 +0200

    Added Try and Catch to handle exceptions

--- ReadTextFileWhile/Program.cs
+++ ReadTextFileWhile/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,19 +11,50 @@ namespace ReadTextFileWhile
     {
         static void Main(string[] args)
         {
-            StreamReader myReader = new StreamReader("Values.txt");
-            string Line = "";
 
-            while (Line != null)
+            try
             {
-                Line = myReader.ReadLine();
-                if (Line != null)
-                    Console.WriteLine(Line);
+
+                StreamReader myReader = new StreamReader("Values.txt");
+                string Line = "";
+
+                while (Line != null)
+                {
+                    Line = myReader.ReadLine();
+                    if (Line != null)
+                        Console.WriteLine(Line);
+                }
+                myReader.Close();
+                Console.ReadLine();
             }
-            myReader.Close();
+
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Couldn't find the file. Are you sure the Directory exists?");
+            }
+
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Couldn't find the file. Are you sure the File exists?");
+
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh oh...now you broke it...!!! : {0}", e.Message);
+
+            }
+
+            finally {
+                //  Perform any cleanup to roll back the data or close connections
+                //  to files, databases, network etc.
+            }
+
+
             Console.ReadLine();
         }
     }
-}
+    }
 //  To make use of StreamReader you need to add the System.IO namespace! And to make this project run,
 //  set the property called 'Copy to Output Directory' of the Values.txt file to 'Copy Always'
